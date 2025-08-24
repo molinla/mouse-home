5f6a6ce24d4aeecbd1c2b0e4ac99603c7356d4a9
moninla <moninla@126.com>
2025-08-25 00:21:51 +0800

    feat: Add animejs for animations and enhance 3D transformations
    - Introduced animejs as a dependency for improved animations in the project.
    - Added new CSS classes for 3D transformations to enhance visual effects.
    - Updated the Hero component to utilize animejs for animated transitions and draggable elements.
    - Refactored the Features component to dynamically load images for a more flexible layout.
    - Removed unused react.svg asset to streamline the project.

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -1,13 +1,42 @@
-import linux from '../assets/linux.svg'
-import macos from '../assets/mac.svg'
-import windows from '../assets/windows.svg'
+const imagesModules = import.meta.glob('../assets/screenshots/*.png', {
+  eager: true,
+})
 
 function WhatCanWeDo() {
+  const images = Object.entries(imagesModules).map(
+    ([_, module]) => (module as any).default as string
+  )
+  console.log(images)
   return (
     <section className="w-full min-h-screen bg-white dark:bg-transparent flex flex-col items-center justify-center gap-6 md:gap-12 p-4 md:p-12">
       <h1 className="max-w-7xl mx-auto text-3xl md:text-5xl lg:text-7xl font-bold leading-tight md:leading-[4] text-[#1d1d1d] dark:text-white text-center">
         黑匣边缘计算设备能做什么？
       </h1>
+      <section className="w-full h-full flex items-center justify-between gap-36 mt-24">
+        <div className="flex flex-col gap-4">
+          <h2 className="text-2xl font-bold">AI边缘检测计算</h2>
+          <p className="text-lg">
+            可以实现任何操作系统的所有桌面视觉的自动化，图册、桌面图像检测、网页图像检测，或你日常使用的任何其他应用程序的图像检测。
+          </p>
+        </div>
+        <div className="relative w-full px-24 pt-24 flex items-center justify-center">
+          <div className="relative transform-3d origin-left rotate-y-12">
+            {images.map((image, index) => (
+              <img
+                src={image}
+                className="first:relative not-first:absolute px-[20%] pt-[10%] pb-0"
+                style={{
+                  left: `${index * 16}px`,
+                  top: `-${index * 12}px`,
+                  zIndex: 10 - index,
+                }}
+                alt="screenshot"
+                key={image}
+              />
+            ))}
+          </div>
+        </div>
+      </section>
     </section>
   )
 }
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,21 +1,86 @@
+import {
+  createDraggable,
+  createScope,
+  createSpring,
+  createTimeline,
+  type Scope,
+} from 'animejs'
+import { useEffect, useRef } from 'react'
 import device from '../assets/imgs/device.png'
 import LightRays from '../hero-sections/light-rays'
 import TextType from '../hero-sections/text-type'
 
 export default function Hero() {
+  const root = useRef<HTMLDivElement>(null)
+  const scope = useRef<Scope | null>(null)
+
+  useEffect(() => {
+    scope.current = createScope({ root }).add(() => {
+      const timeline = createTimeline()
+      const spring = createSpring({ mass: 0.5, stiffness: 100, damping: 20 })
+      timeline
+        .label('hero-section')
+        .add('.title', {
+          y: {
+            from: '100%',
+            to: '0',
+            duration: 600,
+            ease: spring,
+          },
+          opacity: {
+            from: 0,
+            to: 1,
+            duration: 600,
+            delay: 100,
+          },
+        })
+        .add('.slogan', {
+          y: {
+            from: '6rem',
+            to: '0',
+            duration: 600,
+            ease: spring,
+          },
+          opacity: {
+            from: 0,
+            to: 1,
+            duration: 600,
+            delay: 100,
+          },
+        })
+        .add('.device', {
+          y: {
+            from: '50%',
+            to: '0',
+            duration: 400,
+            ease: spring,
+          },
+          opacity: {
+            from: 0,
+            to: 1,
+            duration: 600,
+          },
+        })
+
+      createDraggable('.device', {
+        container: [0, 0, 0, 0],
+        releaseEase: createSpring({ stiffness: 300, damping: 30 }),
+      })
+    })
+
+    return () => scope.current?.revert()
+  }, [])
+
   return (
-    <section className="relative h-screen">
+    <section ref={root} className="relative h-screen overflow-hidden">
       <LightRays className="absolute inset-0 z-0" />
       <div className="absolute inset-0 top-0 text-white h-screen flex flex-col gap-8 hd:gap-12 pt-20 items-center justify-center max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center z-10">
-        <h1 className="animated-gradient text-xl md:text-3xl hd:text-5xl font-bold">
-          黑匣HEX·AI Deep learning
-        </h1>
         <TextType
           as="p"
           variableSpeed={{ min: 50, max: 150 }}
           pauseDuration={3500}
           deletingSpeed={30}
-          className="text-3xl font-bold md:text-6xl hd:text-7xl"
+          className="title opacity-0 text-3xl font-bold md:text-6xl hd:text-7xl"
           text={[
             '全面聚合AI边缘计算视觉深度学习',
             '智能边缘设备·实时AI视觉处理',
@@ -24,12 +89,11 @@ export default function Hero() {
             '边缘智能·赋能视觉创新',
           ]}
         />
-        <p className="text-lg font-bold md:text-2xl hd:text-3xl">
-          边缘计算设备·轻松调用并创造AI视觉
+        <p className="slogan opacity-0 animated-gradient text-xl md:text-3xl hd:text-5xl font-bold whitespace-nowrap">
+          黑匣HEX·AI Deep learning
         </p>
         <img
-          className="mt-10 hd:mt-20 hd:scale-125"
-          draggable={false}
+          className="opacity-0 device mt-10 hd:mt-20 hd:scale-125"
           width={723}
           height={407}
           src={device}
@@ -38,6 +102,30 @@ export default function Hero() {
       </div>
       <div className="absolute z-10 bg-[#F4CD2E] bottom-0 blur-[541px] left-0 size-[340px] rounded-full opacity-30"></div>
       <div className="absolute z-10 bg-[#ED9BFC] bottom-0 blur-[541px] right-0 size-[340px] rounded-full opacity-30"></div>
+      <nav className="absolute bottom-4 left-0 w-full flex items-center justify-center z-20">
+        <button
+          type="button"
+          className="bg-transparent border-none animate-bounce"
+        >
+          <svg
+            role="presentation"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="text-white animate-pulse"
+          >
+            <path
+              d="M6 9l6 6 6-6"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </nav>
     </section>
   )
 }
